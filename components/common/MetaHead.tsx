c29c79e4ce32a56ac1cc6aef64216da23d17d9e0
uu5474 <uu5474@gmail.com>
2022-09-24 15:11:18 +0900

    feat: naver-site-verification 추가

--- components/common/MetaHead.tsx
+++ components/common/MetaHead.tsx
@@ -12,6 +12,7 @@ const MetaHead = ({ title, description }: MetaHeadProps) => (
     <meta name="description" content={description || seo.description} />
     <meta property="og:title" content={title || seo.title} />
     <meta property="og:type" content="website" />
+    <meta name="naver-site-verification" content="4ea4d4d6650eead7711fe5b7b37e71ddcda3c63b" />
     {/* <meta property="og:image" content={image} /> */}
   </Head>
 );
